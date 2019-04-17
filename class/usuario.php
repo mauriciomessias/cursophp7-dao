18e5b4b1ccc9828dfe0469fb6cb88be26a6855c8
Mauricio Messias <mauricio_messias@uol.com.br>
2019-04-17 13:33:16 -0300

    Listas

--- class/usuario.php
+++ class/usuario.php
@@ -43,6 +43,7 @@
 			$this->dtcadastro = $value;
 		}
 
+		// ***** LISTA APENAS 1 USUARIO *****
 		public function loadById($id)
 		{
 			$sql = new Sql();
@@ -54,6 +55,39 @@
 				$this->setDeslogin($row['deslogin']);
 				$this->setDessenha($row['dessenha']);
 				$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			}
+		}
+
+		// ***** LISTA TODOS USUARIOS *****
+		public static function getList()
+		{
+			$sql = new Sql();
+			return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+		}
+
+		// ***** LOCALIZA UM USUARIO *****
+		public static function search($login)
+		{
+			$sql = new Sql();
+			return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(':SEARCH'=>"%".$login."%"
+			));
+		}
+
+		// ***** MOSTRA USUARIO QUE PARAMETROS ESTEJAM OK *****
+		public function login($login, $password)
+		{
+			$sql = new Sql();
+			$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(":LOGIN"=>$login, ":PASSWORD"=>$password
+		));
+			if (count($results) > 0)
+			{
+				$row = $results[0];
+				$this->setIdusuario($row['idusuario']);
+				$this->setDeslogin($row['deslogin']);
+				$this->setDessenha($row['dessenha']);
+				$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			} else {
+				throw new Exception("Login e/ou Senha inválidos!!!");
 			}
 		}
 
--- index.php
+++ index.php
@@ -8,11 +8,28 @@
 	// (1) Chama PHP de configurações
 	require_once("config.php");
 
-	// Instancia Usuario
+	/*
+	// EXEMPLO 1 - Mostra usuario escolhido no parâmetro
 	$root = new Usuario();
-
-	// Mostra usuario
 	$root->loadById(3);
 	echo $root;
+	*/
+
+	/*
+	// EXEMPLO 2 - Mostra uma lista de usuários
+	$lista = Usuario::getList(); 
+	echo json_encode($lista);
+	*/
+
+	/*
+	// EXEMPLO 3 - Procura usuários com parte do nome definido no parâmetro
+	$search = Usuario::search("Mess"); 
+	echo json_encode($search);
+	*/
+
+	// EXEMPLO 4 - SÓ MOSTRA SE LOGIN E SENHA SÃO VALIDOS
+	$usuario = new Usuario(); 
+	$usuario->login("Mauricio Messias", "12345");
+	echo $usuario;
 
 ?>
